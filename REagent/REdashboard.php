d40fe9cebbb3fa5bfcbd07c9caf71269dd29832c
mxmliew <michelle.apple2545@gmail.com>
2024-05-02 15:01:34 +0800

    correct the listing margins

--- REagent/REdashboard.php
+++ REagent/REdashboard.php
@@ -42,89 +42,90 @@
     </ul>
     </nav>
 
-    <!-- Search Bar -->
-    <div class="container mt-5">
-        <div class="search-container">
-            <div class="searchbox">
-                <p><b>Search Property Listing</b></p>
-                <input type="text" id="searchBox" name="searchBox" placeholder="Search.." size="40">
-            </div>
-        
+<!-- Search Bar -->
+<div class="container mt-5">
+    <div class="search-container">
+        <div class="searchbox">
+            <p><b>Search Property Listing</b></p>
+            <input type="text" id="searchBox" name="searchBox" placeholder="Search..">
         </div>
     </div>
+</div>
 
 
-    <!-- Property Listings -->
-    <div class="suspend-container">
-    <div class="selectAll"> <!-- top border -->      
-    <div class="suspendList">
-    <div class="container mt-5">
-        <div class="row">
-            <!-- Sample Property Card -->
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <img class="card-img-top" src="placeholder-image.jpg" alt="Property Image">
-                    <div class="card-body">
-                        <h5 class="card-title">663C Jurong West Street 65</h5>
-                        <p class="card-text">$550,000 - 1578 sqft 3 bed 2 bathroom</p>
-                        <a href="#" class="btn btn-primary">View Details</a>
+
+<!-- Property Listings -->
+<div class="container mt-5">
+    <div class="listing-container">
+        <div class="scrollList">
+            <div class="row">
+                <!-- Sample Property Card -->
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img class="card-img-top" src="placeholder-image.jpg" alt="Property Image">
+                        <div class="card-body">
+                            <h5 class="card-title">663C Jurong West Street 65</h5>
+                            <p class="card-text">$550,000 - 1578 sqft 3 bed 2 bathroom</p>
+                            <a href="#" class="btn btn-primary">View Details</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <!-- Repeat the above block for each property, ensure each is within a col-md-4 -->
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <img class="card-img-top" src="placeholder-image.jpg" alt="Property Image">
-                    <div class="card-body">
-                        <h5 class="card-title">663C Jurong West Street 65</h5>
-                        <p class="card-text">$550,000 - 1578 sqft 3 bed 2 bathroom</p>
-                        <a href="#" class="btn btn-primary">View Details</a>
+                <!-- Repeat the above block for each property, ensure each is within a col-md-4 -->
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img class="card-img-top" src="placeholder-image.jpg" alt="Property Image">
+                        <div class="card-body">
+                            <h5 class="card-title">663C Jurong West Street 65</h5>
+                            <p class="card-text">$550,000 - 1578 sqft 3 bed 2 bathroom</p>
+                            <a href="#" class="btn btn-primary">View Details</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <img class="card-img-top" src="placeholder-image.jpg" alt="Property Image">
-                    <div class="card-body">
-                        <h5 class="card-title">663C Jurong West Street 65</h5>
-                        <p class="card-text">$550,000 - 1578 sqft 3 bed 2 bathroom</p>
-                        <a href="#" class="btn btn-primary">View Details</a>
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img class="card-img-top" src="placeholder-image.jpg" alt="Property Image">
+                        <div class="card-body">
+                            <h5 class="card-title">663C Jurong West Street 65</h5>
+                            <p class="card-text">$550,000 - 1578 sqft 3 bed 2 bathroom</p>
+                            <a href="#" class="btn btn-primary">View Details</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <img class="card-img-top" src="placeholder-image.jpg" alt="Property Image">
-                    <div class="card-body">
-                        <h5 class="card-title">663C Jurong West Street 65</h5>
-                        <p class="card-text">$550,000 - 1578 sqft 3 bed 2 bathroom</p>
-                        <a href="#" class="btn btn-primary">View Details</a>
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img class="card-img-top" src="placeholder-image.jpg" alt="Property Image">
+                        <div class="card-body">
+                            <h5 class="card-title">663C Jurong West Street 65</h5>
+                            <p class="card-text">$550,000 - 1578 sqft 3 bed 2 bathroom</p>
+                            <a href="#" class="btn btn-primary">View Details</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <img class="card-img-top" src="placeholder-image.jpg" alt="Property Image">
-                    <div class="card-body">
-                        <h5 class="card-title">663C Jurong West Street 65</h5>
-                        <p class="card-text">$550,000 - 1578 sqft 3 bed 2 bathroom</p>
-                        <a href="#" class="btn btn-primary">View Details</a>
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img class="card-img-top" src="placeholder-image.jpg" alt="Property Image">
+                        <div class="card-body">
+                            <h5 class="card-title">663C Jurong West Street 65</h5>
+                            <p class="card-text">$550,000 - 1578 sqft 3 bed 2 bathroom</p>
+                            <a href="#" class="btn btn-primary">View Details</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <img class="card-img-top" src="placeholder-image.jpg" alt="Property Image">
-                    <div class="card-body">
-                        <h5 class="card-title">663C Jurong West Street 65</h5>
-                        <p class="card-text">$550,000 - 1578 sqft 3 bed 2 bathroom</p>
-                        <a href="#" class="btn btn-primary">View Details</a>
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img class="card-img-top" src="placeholder-image.jpg" alt="Property Image">
+                        <div class="card-body">
+                            <h5 class="card-title">663C Jurong West Street 65</h5>
+                            <p class="card-text">$550,000 - 1578 sqft 3 bed 2 bathroom</p>
+                            <a href="#" class="btn btn-primary">View Details</a>
+                        </div>
                     </div>
                 </div>
+                <!-- Continue adding cards as needed -->
             </div>
-            <!-- Continue adding cards as needed -->
         </div>
-    </div></div></div></div>
+    </div>
+</div>
 
 
 </body>
