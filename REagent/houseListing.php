6f806d372eb2cf0088c57d2d0951b0267e127639
mxmliew <michelle.apple2545@gmail.com>
2024-05-02 23:30:58 +0800

    create removeListing

--- REagent/houseListing.php
+++ REagent/houseListing.php
@@ -58,7 +58,7 @@
         </div>
         <div class="user-buttons">
             <a href="createListing.php" class="button">Create Listing</a>
-            <a href="" class="button">Remove Listing</a>
+            <a href="removeListing.php" class="button">Remove Listing</a>
             <a href="" class="button">Update Listing</a>
         </div>
     </div>
