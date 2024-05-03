422ab2062f0f567ee653a16df8866ccb56691c77
pkarthikeyan98 <pkarthikeyan98@gmail.com>
2024-05-03 21:56:54 +0800

    changes to makes the login work
    I have removed the inbuilt js for the login function and replaced it with the code in the LoginController which was initially userController (thanks to michelle) !!

--- DBC/Database.php
+++ DBC/Database.php
@@ -6,21 +6,11 @@ class Database {
     private $password = '';
     private $conn;
 
-    // Get the database connection
     public function getConnection() {
-        $this->conn = null;
-
-        try {
-            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
-
-            // Check connection
-            if ($this->conn->connect_error) {
-                throw new Exception("Connection failed: " . $this->conn->connect_error);
-            }
-        } catch (Exception $e) {
-            echo "Error: " . $e->getMessage();
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
         }
-
         return $this->conn;
     }
 }
