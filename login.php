4e9a83ae2293bf66e083a8b2ece7b8d006cc4359
mxmliew <michelle.apple2545@gmail.com>
2024-05-09 23:42:20 +0800

    fixed the login alignment

--- login.php
+++ login.php
@@ -42,27 +42,51 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <div class="container mt-5">
     <div class="login-container">
-        <h1>Login</h1>
+        <h1><b>Real Estate</b></h1>
         <?php if ($login_err != "") echo '<div class="alert alert-danger">' . $login_err . '</div>'; ?>
         <form id="login-form" method="POST" action="login.php">
             <div class="form-group">
-                <label for="userType">User:</label>
-                <select class="form-control" id="userType" name="userType">
-                    <option>Admin</option>
-                    <option>Buyer</option>
-                    <option>Seller</option>
-                    <option>Agent</option>
-                </select>
+                <div class="row">
+                    <div class="col-3">
+                        <label for="userType">User:</label>
+                    </div>
+                    <div class="col">
+                    <select class="form-control" id="userType" name="userType">
+                        <option>Admin</option>
+                        <option>Buyer</option>
+                        <option>Seller</option>
+                        <option>Agent</option>
+                    </select>
+                    </div>
+                </div>
             </div>
             <div class="form-group">
-                <label for="loginID">Login ID:</label>
-                <input type="text" class="form-control" id="loginID" name="loginID" required>
+                <div class="row">
+                    <div class="col-3">
+                        <label for="loginID">Login ID:</label>
+                    </div>
+                    <div class="col">
+                        <input type="text" class="form-control" id="loginID" name="loginID" required>
+                    </div>
+                </div>
             </div>
             <div class="form-group">
-                <label for="password">Password:</label>
-                <input type="password" class="form-control" id="password" name="password" required>
+                <div class="row">
+                    <div class="col-3">
+                        <label for="password">Password:</label>
+                    </div>
+                    <div class="col">
+                        <input type="password" class="form-control" id="password" name="password" required>
+                    </div>
+                </div>
             </div>
-            <button type="submit" class="btn btn-primary">Login</button>
+            <div class="form-group">
+                <div class="row" style="display: flex; justify-content: center;"> 
+                    <div class="col-3 m-auto">
+                        <button type="submit" class="btn btn-primary btn-block">Login</button>
+                    </div>
+                </div>
+            </div>
         </form>
     </div>
 </div>
