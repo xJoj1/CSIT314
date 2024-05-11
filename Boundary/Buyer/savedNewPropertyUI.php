1f670ad26c2e5a59959bc26ec336dd5ffbb09981
pkarthikeyan98 <pkarthikeyan98@gmail.com>
2024-05-11 21:30:36 +0800

    i have made the heart work again :hatched_chick:

--- Boundary/Buyer/savedNewPropertyUI.php
+++ Boundary/Buyer/savedNewPropertyUI.php
@@ -105,25 +105,30 @@
     <div class="scrollList">
         <div class="row">
             <?php foreach ($properties as $property): ?>
+
+                
                 <div class="col-md-4 mb-4">
                     <div class="card">
                         <img class="card-img-top" src="<?php echo $property['image_url']; ?>" alt="Property Image">
                         <div class="card-body">
                             <h5 class="card-title"><?php echo $property['address']; ?></h5>
+
+
                             <p class="card-text">
-                                <?php echo '$' . number_format($property['price']) . ' - ' . $property['size'] . ' sqft ' . $property['beds'] . ' bed ' . $property['baths'] . ' bathroom'; ?>
-                            </p>
-                            <a href="viewNewPropertyDetails.php?id=<?php echo $property['id']; ?>&increment_views=1" class="btn btn-primary">View Details</a>
+                <?php echo '$' . number_format($property['price']) . ' - ' . htmlspecialchars($property['size']) . ' sqft ' . htmlspecialchars($property['beds']) . ' bed ' . htmlspecialchars($property['baths']) . ' bathroom'; ?>
+            </p>
+
+            <a href="viewNewPropertyDetails.php?id=<?php echo htmlspecialchars($property['id']); ?>&increment_views=1" class="btn btn-primary">View Details</a>
                         </div>
                         <div class="card-footer">
                             <!-- Form for toggling the bookmark -->
-                            <form method="POST" action="savedNewPropertyUI.php">
-                                <input type="hidden" name="propertyId" value="<?php echo htmlspecialchars($property['id']); ?>">
-                                <input type="hidden" name="bookmark" value="<?php echo $property['bookmark'] == '1' ? '0' : '1'; ?>">
-                                <button type="submit" class="btn btn-link p-0">
-                                    <i class="fa-heart <?php echo $property['bookmark'] == '1' ? 'fas' : 'far'; ?> favorite-icon"></i>
-                                </button>
-                            </form>
+                        <form method="POST" action="savedNewPropertyUI.php">
+                            <input type="hidden" name="propertyId" value="<?php echo htmlspecialchars($property['id']); ?>">
+                            <input type="hidden" name="bookmark" value="<?php echo isset($property['bookmark']) ? ($property['bookmark'] == '1' ? '0' : '1') : '0'; ?>">
+                            <button type="submit" class="btn btn-link p-0">
+                                <i class="fa-heart <?php echo isset($property['bookmark']) && $property['bookmark'] == '1' ? 'fas' : 'far'; ?> favorite-icon"></i>    
+                        </button>
+                        </form>
                         </div>
                     </div>
                 </div>
@@ -145,7 +150,7 @@
     function toggleFavorite(element) {
         element.classList.toggle('far');
         element.classList.toggle('fas');
-      
+     
     }
 
     document.addEventListener('DOMContentLoaded', function() {
