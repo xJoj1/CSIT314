c2f3255ee977db7775f77b7dcb62a81f28ce8333
George <georgepek196@yahoo.com>
2024-05-15 01:45:10 +0800

    Update mortgageCalculatorUI.php

--- Boundary/Buyer/mortgageCalculatorUI.php
+++ Boundary/Buyer/mortgageCalculatorUI.php
@@ -43,7 +43,6 @@
         Welcome Buyer
       </a>
       <div class="dropdown-menu dropdown-menu-right" aria-labelledby="adminMenu">
-        <a class="dropdown-item" href="#">Profile</a>
         <a class="dropdown-item" href="../../logout.php">Logout</a> <!-- Link to logout.php -->
       </div>
     </li>
